33cd5e0511c261cdaf012ac6b5e3542dba361cd2
tangb4c <tangb4c@gmail.com>
2016-09-12 11:34:02 +0800

    v1

--- scripts/boot/auto_run.cmd
+++ scripts/boot/auto_run.cmd
@@ -1,6 +1,3 @@
-@net use Z: /USER:blaketang \\10.6.207.84\blaketang 131457
-@net use A: /USER:blaketang \\10.6.207.84\blaketang\qzone\application 131457
-@net use Y: /USER:blaketang \\10.6.207.61\blaketang 131457
 del R:\temp\*.* /F /S /A /Q
 del R:\temp\* /F /S /A /Q
 RD /S /Q R:\temp
@@ -9,5 +6,5 @@ MD R:\Temp
 MD R:\Cache\Firefox
 MD R:\Cache\IExplorer
 MD R:\Cache\Chrome
-start /MIN D:\Tools\boot\windows_hotkey.ahk
+start /MIN windows_hotkey.ahk
 
